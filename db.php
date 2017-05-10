a9c226d7143df2a0f2dbe021c41ec7f3587070fa
vanessacarmen <vanessacarmenq@gmail.com>
2017-05-10 12:54:14 -0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+  $servername = "us-cdbr-iron-east-03.cleardb.net";
+  $username = "b74ba3320e82ec";
+  $password = "2d194843";
+  $dbname = "ad_bb6ddb2f34daf52";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } ?>
